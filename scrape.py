72ae5897dcf4956be8eb620fe3131331150b0b46
Shree3245 <Shreevenkr@gmail.com>
2020-11-02 10:49:43 +0530

    Changed requirements

--- scrape.py
+++ scrape.py
@@ -1,26 +1,62 @@
-import tweepy
-from pprint import pprint
-consumer_key = "jLSfYIkrNyp4vCHOYTwfLQX2K"
-consumer_secret = "vYLeUUGojuhkItNKz58FwjwZxOTS5fh4PuTKx2vZOKlU4UziwZ"
-access_token = "825369963775004672-WnMKmuLV3eKRPPtykWYBq1cAStaB8mx"
-access_token_secret = "9PFNd33jxjsWqALXXglhKH6V9KexDTy4vxIyXuSVjwSSn"
-#bearer = 'AAAAAAAAAAAAAAAAAAAAAL4lIAEAAAAAxq5gNLRhCIg7Sx30afC0OgtQ73s%3DDZCGCq3YlxXMRywFvyMmbFEjNtnpszAVcVaBwfGxAu5px3bHIV'
-# OAUTH_KEYS = {'consumer_key':ckey, 'consumer_secret':csecret,'access_token_key':atoken, 'access_token_secret':asecret}
+import tweepy                             #Module used to retrieve tweets
+from pprint import pprint                 #Module used for clarity behind prints
+from geopy.geocoders import Nominatim     #Module used to turn location into geoCoordinates
 
 
+def get_geoCode(location):
+    location = geolocator.geocode(location)
+    location= (location.latitude, location.longitude)
+    return location
 
-# auth = tweepy.OAuthHandler(OAUTH_KEYS['consumer_key'], OAUTH_KEYS['consumer_secret'])
-# api = tweepy.API(auth)
+def get_tweet(query,location=None):
+    #Instantiate tweepy model
+    api = tweepy.API(auth)
+    
+    #Gather and retrieve tweets based upon only query
+    if not location:
+        tweets=tweepy.Cursor(api.search, q=query).items(1000)
+        outputOfTweets =[]
+        for tweet in tweets:
+            outputOfTweets.append(tweet._json['text'])
+        return outputOfTweets
+        
+    #get the geo data
+    geo = get_geoCode(location)
+    print(geo[0])
+    geo = "{},{},1km".format(geo[0],geo[1])
+    #Crawl the recent twitter for tweets
+    print(geo)
+    tweets=tweepy.Cursor(api.search, q=query,geocode=geo).items(1000)
+    outputOfTweets =[]
+    for tweet in tweets:
+        outputOfTweets.append(tweet._json['text'])
+    return outputOfTweets
 
-# cricTweet = tweepy.Cursor(api.search, q='cricket', geocode="-22.9122,-43.2302,1km").items(10)
+if __name__ == "__main__":
+    #Authenticate program using api
+    #Use users auth keys
+    consumer_key = "jLSfYIkrNyp4vCHOYTwfLQX2K"
+    consumer_secret = "vYLeUUGojuhkItNKz58FwjwZxOTS5fh4PuTKx2vZOKlU4UziwZ"
+    access_token = "825369963775004672-WnMKmuLV3eKRPPtykWYBq1cAStaB8mx"
+    access_token_secret = "9PFNd33jxjsWqALXXglhKH6V9KexDTy4vxIyXuSVjwSSn"
 
-# for tweet in cricTweet:
-#    print (tweet.created_at, tweet.text, tweet.lang)
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+    #Authenticate the api
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    #Authenticate the geolocater
+    geolocator = Nominatim(user_agent="globalSearch")
 
-x= tweepy.Cursor(api.user_timeline, q='cricket', geocode="-22.9122,-43.2302,1km").pages(1)
-pprint(x)
-
-print(hello)
+    ### GET USER INPUT ###
+    
+    query = input("What Topic would you like to scrape: ")
+    location = input("Would you like to gather data from a certain location (Y/N): ")
+    if location.lower() == "y":
+        print("Where would you like to gather data from:")
+        print("For example 'Hyderabad', or 'London' or even a specific address like: ")
+        print("1A Survey No: 62, Bahadurpally, Hyderabad, Telangana 500043? \n")
+        location = input("Please enter: ") 
+    else:
+        location= None
+    #start the model
+    pprint(get_tweet(query, location))
